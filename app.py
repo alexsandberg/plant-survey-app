8eacb3628ba808d5c5b7af97fb5ac989a03e2fd0
Alex Sandberg <alexsandberg7@gmail.com>
2020-01-08 16:57:01 -0700

    Add routes for index and GET /plants

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, abort, jsonify, render_template, redirect
 from models import setup_db
 from flask_cors import CORS
+from models import Plant, PlantInstance
 
 
 def create_app(test_config=None):
@@ -23,11 +24,32 @@ def create_app(test_config=None):
                              'GET,PUT,POST,DELETE,OPTIONS')
         return response
 
+    # ------------------------------------
     # ROUTES
+    # ------------------------------------
 
+    # home page route handler
     @app.route('/')
     def index():
-        pass
+        return render_template('pages/home.html')
+
+    @app.route('/plants')
+    def plants():
+
+        # get all plants from database
+        plants = Plant.query.all()
+
+        plants_formatted = []
+
+        # format each plant
+        for plant in plants:
+            plants_formatted.append(plant.format())
+
+        # return plants
+        return jsonify({
+            'success': True,
+            'plants': plants_formatted
+        })
 
     return app
 
