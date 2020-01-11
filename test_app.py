489cbc2f4ad40c23b79d2e717022efa945d6a55a
Alex Sandberg <alexsandberg7@gmail.com>
2020-01-11 13:28:26 -0700

    Initial setup of test_app.py

---
+++ test_app.py
@@ -0,0 +1,36 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
+
+from app import create_app
+from models import setup_db, Plant, Observation
+
+
+class TriviaTestCase(unittest.TestCase):
+    """This class represents the trivia test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = "plant_survey_test"
+        self.database_path = "postgres://{}/{}".format(
+            'localhost:5432', self.database_name)
+        setup_db(self.app, self.database_path)
+
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.create_all()
+
+    def tearDown(self):
+        """Executed after reach test"""
+        pass
+
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
